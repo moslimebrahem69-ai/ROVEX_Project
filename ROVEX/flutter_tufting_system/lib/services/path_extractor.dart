cf7284f71f32b45dcbd70c3002c74d19aeb4e3e3
Moslim Ebrahem <moslimebrahem69-ai@gmail.com>
2026-08-29 23:34:59 +0300

    refactor: adjust color merge distance constant for clarity

--- ROVEX/flutter_tufting_system/lib/services/path_extractor.dart
+++ ROVEX/flutter_tufting_system/lib/services/path_extractor.dart
@@ -26,7 +26,7 @@ import '../models/design.dart';
 class PathExtractor {
   static const int _maxSide = 420;
   static const int _maxColors = 8;
-  static const int _colorMergeDist = 46; // pixels lighter than this = background/canvas
+    static const int _colorMergeDist = 46; // pixels lighter than this = background/canvas
 
   static List<TuftPoint> extractFromBytes(
     Uint8List bytes, {
